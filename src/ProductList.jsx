9eea4ed281495bc39ff85401d2d3ebdcbd360d18
Yifei Yan <yifeiyan@umich.edu>
2024-09-19 22:50:41 -0400

    typo handleAddTocart

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -7,7 +7,7 @@ function ProductList() {
     const [showCart, setShowCart] = useState(false); 
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
     const [addedToCart, setAddedToCart] = useState({});
-    const handleAddedToCart = (product) => {
+    const handleAddToCart = (product) => {
         dispatch(addItem(product));
         setAddedToCart((prevState)=>({
             ...prevState,
