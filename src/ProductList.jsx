0944e86cac841a9582066da6105b5e74b5bd1a34
Yifei Yan <yifeiyan@umich.edu>
2024-09-19 22:41:28 -0400

    add product cards with double map loop

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -268,7 +268,22 @@ const handlePlantsClick = (e) => {
         </div>
         {!showCart? (
         <div className="product-grid">
-
+        {plantsArray.map((category, index) => (
+        <div key={index}>
+            <h1><div>{category.category}</div></h1>
+            <div className="product-list">
+                {category.plants.map((plant, plantIndex) => (
+                <div className="product-card" key={plantIndex}>
+                    <div className="product-title">{plant.name}</div>
+                    <img className="product-image" src={plant.image} alt={plant.name} />
+                    <div className='product-price'>{plant.cost}</div>
+                    <div>{plant.description}</div>
+                    <button  className="product-button" onClick={() => handleAddToCart(plant)}>Add to Cart</button>
+                </div>
+                ))}
+            </div>
+    </div>
+    ))}
 
         </div>
  ) :  (
