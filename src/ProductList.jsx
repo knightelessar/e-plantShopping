4a52ed9a47158a4026a445b75d4aef38cad52d8c
Yifei Yan <yifeiyan@umich.edu>
2024-09-19 22:49:13 -0400

    handleAddedToCart and dispatch function

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,10 +1,19 @@
 import React, { useState,useEffect } from 'react';
 import './ProductList.css'
 import CartItem from './CartItem';
+import { addItem } from './CartSlice';
+
 function ProductList() {
     const [showCart, setShowCart] = useState(false); 
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
-
+    const [addedToCart, setAddedToCart] = useState({});
+    const handleAddedToCart = (product) => {
+        dispatch(addItem(product));
+        setAddedToCart((prevState)=>({
+            ...prevState,
+            [product.name]:true
+        }));
+    };
     const plantsArray = [
         {
             category: "Air Purifying Plants",
